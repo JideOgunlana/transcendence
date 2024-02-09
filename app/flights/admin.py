fcb7600c5b37abe60478c4d648875177cffbdc67
Dennis <antonikden@gmail.com>
2024-02-09 05:01:00 +0100

    commit

--- app/flights/admin.py
+++ app/flights/admin.py
@@ -2,3 +2,5 @@ from django.contrib import admin
 
 from .models import Flight, Airport
 
+admin.site.register(Airport)
+admin.site.register(Flight)
--- app/flights/views.py
+++ app/flights/views.py
@@ -6,3 +6,9 @@ def index(request):
     return render(request, "flights/index.html", {
 		"flights": models.Flight.objects.all()
 	})
+
+def flight(request, flight_id):
+    flight = models.Flight.objects.get(pk=flight_id)
+    return render(request, "flights/flight.html", {
+		"flight":flight
+	})
