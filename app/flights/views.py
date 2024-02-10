ed622e7e64bb6dfd8bbf563616ee8dea65a61371
Dennis Antonik <antonikden@gmail.com>
2024-02-10 05:33:21 +0100

    commit

--- app/flights/views.py
+++ app/flights/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from . import models
 
 # Create your views here.
@@ -10,5 +12,13 @@ def index(request):
 def flight(request, flight_id):
     flight = models.Flight.objects.get(pk=flight_id)
     return render(request, "flights/flight.html", {
-		"flight":flight
+		"flight":flight,
+		"passengers": flight.passenger.all()
 	})
+
+def book(request, flight_id):
+    if request.method == "POST":
+        flight = models.Flight.objects.get(pk=flight_id)
+        passenger = models.Passenger.objects.get(pk=int(request.POST["passenger"]))
+        passenger.flights.add(flight)
+        return HttpResponseRedirect(reverse("flight", args=(flight.id,)))
