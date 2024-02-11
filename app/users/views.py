34c64e341f3ef3b696ad991b34d6068b6f25ec3a
Dennis Antonik <antonikden@gmail.com>
2024-02-11 20:29:32 +0100

    commit

--- app/users/views.py
+++ app/users/views.py
@@ -25,6 +25,7 @@ def login_view(request):
 	return render(request, "users/login.html")
 
 def logout_view(request):
+	logout(request)
 	return render(request, "users/login.html", {
 		"message": "Logged out."
 	})
