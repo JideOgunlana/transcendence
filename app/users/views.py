f363d8fd764595f3b4603b1c5cb2d8fd32156f24
Dennis Antonik <antonikden@gmail.com>
2024-02-11 17:40:07 +0100

    commit

--- app/users/views.py
+++ app/users/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+from . import models
 
-# Create your views here.
+
+def index(request):
+	return HttpResponse("Hello")
+
+def login_view(request):
+	return HttpResponse("Hello")
+
+def logout_view(request):
+	return HttpResponse("Hello")
