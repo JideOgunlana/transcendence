6b7b6d1652c54292723f5862c643caec9af767f3
Dennis Antonik <antonikden@gmail.com>
2024-02-11 20:21:10 +0100

    commit

--- app/users/views.py
+++ app/users/views.py
@@ -8,6 +8,7 @@ from . import models
 def index(request):
 	if not request.user.is_authenticated:
 		return HttpResponseRedirect(reverse("login"))
+	return render(request, "users/user.html")
 
 def login_view(request):
 	if request.method == "POST":
@@ -16,7 +17,7 @@ def login_view(request):
 		user = authenticate(request,username=username, password=password)
 		if user is not None:
 			login(request, user)
-			return HttpResponseRedirect(reverse("index"))
+			return HttpResponseRedirect(reverse("users:index"))
 		else:
 			return render(request, "users/login.html", {
 				"message": "Invalid credentials."
@@ -24,4 +25,6 @@ def login_view(request):
 	return render(request, "users/login.html")
 
 def logout_view(request):
-	return HttpResponse("Hello")
+	return render(request, "users/login.html", {
+		"message": "Logged out."
+	})
