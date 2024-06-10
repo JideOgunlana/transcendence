f3c75cd614b8de3fbbc7d0561689c6a96e4e625e
TaleaX <sophie.dehne01@gmail.com>
2024-06-10 22:10:32 +0200

    ball accuracy return helper

--- src/containers/Games/PongGame/Enums.js
+++ src/containers/Games/PongGame/Enums.js
@@ -5,9 +5,15 @@ export const LightTypes = Object.freeze({
 	Point: Symbol(3)
 })
 
-export const GameModes = Object.freeze({
-	SinglePlayer: "SinglePlayer",
-	DoublePlayer: "DoublePlayer",
-	MultiPlayer: "MultiPlayer",
-	Tournament: "Tournament"
+// export const GameModes = Object.freeze({
+// 	SinglePlayer: "SinglePlayer",
+// 	DoublePlayer: "DoublePlayer",
+// 	MultiPlayer: "MultiPlayer",
+// 	Tournament: "Tournament"
+// })
+
+export const ReturnStates = Object.freeze({
+	TRUE: Symbol(0),
+	FALSE: Symbol(1),
+	NONE: Symbol(2),
 })
