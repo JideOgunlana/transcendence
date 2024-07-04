edf3326cba0f0c682e7367c4c38605601a38c810
jideOgunlana <jide611@gmail.com>
2024-07-04 22:23:19 +0200

    merge cleanup

--- src/components/index.jsx
+++
@@ -1,9 +0,0 @@
-export { default as GameSelect } from './GameSelect/GameSelect';
-export { default as NextBtn } from './NextBtn/NextBtn';
-export { default as BackBtn } from './BackBtn/BackBtn';
-export { default as HistoryBar } from './HistoryBar/HistoryBar';
-export { default as PlayerList } from './PlayerList/PlayerList';
-export { default as Player } from './Player/Player';
-export { default as StartGameBar } from './StartGameBar/StartGameBar';
-export { default as OptionBtn } from './OptionBtn/OptionBtn';
-export { default as TournamentAlias } from './TournamentAlias/TournamentAlias';
--- src/containers/Games/MemoryGame/index.jsx
+++
@@ -1,3 +0,0 @@
-export { default as MemorySingle } from './MemorySingle';
-export { default as MemoryMulti }  from './MemoryMulti';
-export { default as MemoryTournament }  from './MemoryTournament';
